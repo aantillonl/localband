a04af58530273fb67cf1cc491024e7e4e88161c6
aantillonl <aantillonl@gmail.com>
2020-07-07 00:32:56 +0300

    Updated query template

--- src/searchBoxSlice.js
+++ src/searchBoxSlice.js
@@ -1,6 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import envConfig from './core/env-config.json';
 import qs from 'qs'
 import axios from 'axios'
+
+const environment = process.env.REACT_APP_ENVIRONMENT;
+const apiUrl = envConfig[environment]['api_url'];
 
 const fetchCities = createAsyncThunk(
   'fetchCities',
@@ -8,19 +12,27 @@ const fetchCities = createAsyncThunk(
     if (searchString.length < 4) return {...thunkAPI.getState(), cities: []}
     thunkAPI.dispatch(searchBoxSlice.actions.startFetch())
     const response =  await axios.get(
-      'http://dbpedia.org',
+      apiUrl,
       qs.stringify({
        "default-graph-uri":'http://dbpedia.org',
-        query:`SELECT DISTINCT ?city, ?foafname, ?dbpname, ?population WHERE {
-          ?city rdf:type <http://dbpedia.org/class/yago/City108524735> .
-          OPTIONAL {?city <http://xmlns.com/foaf/0.1/name> ?foafname } .
-          OPTIONAL {?city <http://dbpedia.org/property/name> ?dbpname} .
-          FILTER (regex(?foafname, "${searchString}", "i")|| regex(?dbpname, "^${searchString}","i")) .
-          OPTIONAL {?city <http://dbpedia.org/ontology/populationTotal> ?population}
-        } ORDER BY DESC (?population) LIMIT 5`,
+        query:`
+        PREFIX dbpedia: <http://dbpedia.org/ontology/>
+        PREFIX yago: <http://dbpedia.org/class/yago/>
+
+        SELECT DISTINCT ?city, ?name
+        WHERE {
+            ?city ?t ?type;
+                rdfs:label ?name.
+            OPTIONAL {?city dbo:populationTotal ?population }.
+            FILTER (?type IN (dbo:City, yago:City108524735))
+            FILTER (LANG(?name) = "en")
+            FILTER regex(?name, "^${searchString}", "i")
+        }
+        ORDER BY DESC(?population)
+
+         LIMIT 5`,
         output:'json'
-      }),
-      { headers: { "Access-Control-Allow-Origin": "*" } }
+      })
     )
     return {fetchStatus: 'FINISHED', cities: response.data}
   }
@@ -28,7 +40,7 @@ const fetchCities = createAsyncThunk(
 
 const searchBoxSlice = createSlice({
   name: 'searchBox',
-  initialState: { fetchStatus: 'DEFAULT', cities: [] },
+  initialState: { fetchStatus: 'DEFAULT', cities: [], delayedSearch: null },
   reducers: {
     startFetch: (state) => ({ ...state,  fetchStatus: 'PENDING' }),
   },
